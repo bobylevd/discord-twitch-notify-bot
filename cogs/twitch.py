2fc9cb9da9c6c560c695b18fb59bbdbf08198e81
Dmitry Bobylev <dimas.od@gmail.com>
2021-03-26 17:36:29 +0200

    fixed unkown channel behaviour, now we just wait until a person sets monitoring

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -26,17 +26,18 @@ class TwitchCheck(commands.Cog):
 
         for guild in self.client.guilds:
             streams = get_stream_status(guild.id)
-            channel = self.client.get_channel(int(h_get(guild.id, "channel_id")))
-            if streams:
-                for s in streams:
-                    logger.info(f"found stream by {s.user.login}")
-                    if not h_get(f"{guild.id}:{s.user.login}", "timestamp"):
-                        logger.info(f"stream {s.user.login} has no timestamp, setting to {now_ts}")
-                        h_set(f"{guild.id}:{s.user.login}", "timestamp", now_ts)
-                    if now_ts >= float(h_get(f"{guild.id}:{s.user.login}", "timestamp")):
-                        h_set(f"{guild.id}:{s.user.login}", "timestamp", skip_until_ts)
-                        logger.info(f"able to send notification, updating timestamp from {now_ts} to {skip_until_ts}")
-                        await channel.send(embed=generate_embed(s))
+            if h_get(guild.id, "channel_id") is not None:
+                channel = self.client.get_channel(int(h_get(guild.id, "channel_id")))
+                if streams:
+                    for s in streams:
+                        logger.info(f"found stream by {s.user.login}")
+                        if not h_get(f"{guild.id}:{s.user.login}", "timestamp"):
+                            logger.info(f"stream {s.user.login} has no timestamp, setting to {now_ts}")
+                            h_set(f"{guild.id}:{s.user.login}", "timestamp", now_ts)
+                        if now_ts >= float(h_get(f"{guild.id}:{s.user.login}", "timestamp")):
+                            h_set(f"{guild.id}:{s.user.login}", "timestamp", skip_until_ts)
+                            logger.info(f"able to send notification, updating timestamp from {now_ts} to {skip_until_ts}")
+                            await channel.send(embed=generate_embed(s))
 
     @check.before_loop
     async def before_check(self):
