721c12068227f12789d874155336f736ac253327
Vlad Chekunov <vlad.chekunov@gmail.com>
2022-05-31 02:52:32 +0300

    change twitch api client to twitch.io make the script use only one instance of a twitch.io client fix stream parameter references accordingly

--- twitch_client/twitch_client.py
+++ twitch_client/twitch_client.py
@@ -1,32 +1,33 @@
 import logging
 import os
-from typing import Union
+from typing import List
 
-import twitch
-from twitch.helix import StreamNotFound, Streams
+import twitchio
 
 from database import r
 
 logger = logging.getLogger()
+twitch_io_client = twitchio.Client.from_client_credentials(os.environ['TWITCH_ID'], os.environ['TWITCH_SECRET'], )
 
 
 class TwitchClient:
     def __init__(self, guild_id):
         self.guild_id = guild_id
-        self.helix = twitch.Helix(os.environ['TWITCH_ID'], os.environ['TWITCH_SECRET'])
+        self.api_client = twitch_io_client
 
     @property
     def streamers(self):
         return [s.split(":")[1] for s in r.keys(f"{self.guild_id}:*")]
 
-    def live_streams(self) -> Union[Streams, None]:
+    async def live_streams(self) -> List[twitchio.Stream]:
         """
         Return active streams if any
         :return: `twitch_client.helix.resources.Streams`
         """
-        try:
-            streams = self.helix.streams(user_login=self.streamers)
-            return streams
-        except StreamNotFound:
+
+        if not self.streamers:
+            return []
+        streams = await self.api_client.fetch_streams(user_logins=self.streamers)
+        if not streams:
             logger.debug("No streams found")
-            return None
+        return streams
--- utils.py
+++ utils.py
@@ -1,15 +1,16 @@
 import logging
 
 import discord
-from twitch.helix import Stream
+from twitchio import Stream, User
 
 logger = logging.getLogger()
 
 
-def generate_embed(stream: Stream) -> discord.Embed:
-    embed = discord.Embed(title=f'{stream.user.login} is now live on twitch',
-                          url=f'https://twitch.tv/{stream.user.login}', color=0x00ff00)
-    embed.add_field(name='Playing', value=f'{stream.user.stream.game_name}')
-    embed.description = f'{stream.user.stream.title}'
-    embed.set_thumbnail(url=stream.user.profile_image_url)
+async def generate_embed(stream: Stream) -> discord.Embed:
+    embed = discord.Embed(title=f'{stream.user.name} is now live on twitch',
+                          url=f'https://twitch.tv/{stream.user.name.lower()}', color=0x00ff00)
+    embed.add_field(name='Playing', value=f'{stream.game_name}')
+    embed.description = f'{stream.title}'
+    full_user: User = await stream.user.fetch()
+    embed.set_thumbnail(url=full_user.profile_image)
     return embed
